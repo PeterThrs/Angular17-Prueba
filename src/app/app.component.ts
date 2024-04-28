f425a0c24c1b776a1ea09715255b4b2b0fa52e4e
PeterThrs <peterlopezthrs@gmail.com>
2024-04-27 23:07:16 -0600

    Modificando el primer programa en angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: `
+    h1 {
+      // color: red;
+    }
+  `
 })
 export class AppComponent {
-  title = 'angular-17-app';
+  city = 'Barcelona';
 }
